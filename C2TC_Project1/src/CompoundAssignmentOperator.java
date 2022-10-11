b10986f0564ddfbf716418ed363b9cf2f30f882a
dilipkn007 <dilipkn13082001@gmail.com>
2022-10-11 18:02:48 +0530

    minor changes

---
+++ C2TC_Project1/src/CompoundAssignmentOperator.java
@@ -0,0 +1,15 @@
+
+public class CompoundAssignmentOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=5;
+		x+=5; //x=x+5
+//		x-=5; //x=x-5
+//		x*=5; //x=x*5
+//		x/=5; //x=x/5
+//		x%=5; //x=x-5
+		System.out.println(x);
+	}
+
+}
--- C2TC_Project1/src/TernaryOperator.java
+++ C2TC_Project1/src/TernaryOperator.java
@@ -3,7 +3,8 @@ public class TernaryOperator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		boolean b=(10<20)?true:false;
+		System.out.println(b);
 	}
 
 }
